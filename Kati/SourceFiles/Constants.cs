a762a8c13832829265a46d3e9f29b57a657d345b
ResoTheRed <ltdemg@gmail.com>
2020-05-15 20:59:00 -0700

    Added template code and created GlobalKey class and unit tests

---
+++ Kati/SourceFiles/Constants.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Kati.SourceFiles{
+    /// <summary>
+    /// Class contains a collection of constant values.  It may go to json in the future
+    /// </summary>
+    public class Constants{
+        /*collection of paths for GlobalKeys*/
+        public const string pronoun = "/c/Users/User/Documents/NLG/Kati/SourceFiles/globalKeys.json";
+
+
+    }
+}
